5c2cb1d74e4b255b2bf1d378a684e34a0a62b24e
Rohit Kumar <rk18012003@gmail.com>
2025-01-10 19:20:52 +0530

    file corrected

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import CreateTrip from './create-trip'
+import CreateTrip from './create-trip/index.jsx'
 import Header from './components/custom/Header.jsx'
 import { Toaster } from 'sonner'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 import ViewTrip from './view-trip/[tripID]/index.jsx'
-import MyTrips from './my-trips'
+import MyTrips from './my-trips/index.jsx'
 
 const router = createBrowserRouter([
   {
